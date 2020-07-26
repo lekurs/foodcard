37b1f768bd9ea11b5aed897b9e2c0f728ced52d2
Max <gindre.maxime@gmail.com>
2020-07-26 17:20:24 +0200

    middle-front
    working on payment

--- app/Http/Controllers/Middle/Admin/Menu/MenuShowController.php
+++ app/Http/Controllers/Middle/Admin/Menu/MenuShowController.php
@@ -4,52 +4,40 @@
 namespace App\Http\Controllers\Middle\Admin\Menu;
 
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Middle\AdminMiddleController;
 use App\Repository\StoreRepository;
 use App\Repository\UserFonctionRepository;
 use App\Repository\UserRepository;
 
-class MenuShowController extends Controller
+class MenuShowController extends AdminMiddleController
 {
     /**
      * @var UserRepository $userRepository
      */
     private UserRepository $userRepository;
 
-    /**
-     * @var UserFonctionRepository $userFonctionRepository
-     */
-    private UserFonctionRepository $userFonctionRepository;
+    public function __construct(UserRepository $userRepository, UserFonctionRepository $userFonctionRepository, StoreRepository $storeRepository)
+    {
+        parent::__construct($userFonctionRepository, $storeRepository);
 
-    /**
-     * @var StoreRepository $storeRepository
-     */
-    private StoreRepository $storeRepository;
-
-    /**
-     * MenuShowController constructor.
-     * @param UserRepository $userRepository
-     * @param UserFonctionRepository $userFonctionRepository
-     * @param StoreRepository $storeRepository
-     */
-    public function __construct
-    (
-        UserRepository $userRepository,
-        UserFonctionRepository $userFonctionRepository,
-        StoreRepository $storeRepository
-    ) {
         $this->userRepository = $userRepository;
-        $this->userFonctionRepository = $userFonctionRepository;
-        $this->storeRepository = $storeRepository;
     }
 
     public function show() {
-        $userFonctions = $this->userFonctionRepository->getAll();
         $stores = $this->userRepository->getStoresByUser(request()->user())->stores;
 
         return view('admin.middle.menu.admin_middle_menu_show', [
             'stores' => $stores,
-            'userFonctions' => $userFonctions
+            'userFonctions' => $this->userFonctions
+        ]);
+    }
+
+    public function search() {
+        $stores = $this->userRepository->getStoresByUser(request()->user())->stores;
+
+        return view('admin.middle.menu.admin_middle_menu_search', [
+           'stores' => $stores,
+            'userFonctions' => $this->userFonctions
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,7 @@ Route::group(['prefix' => 'foodcard', 'middleware' => ['auth', 'role']], functio
 
         Route::group(['prefix' => 'ma-carte'], function () {
            Route::get('/', 'Middle\Admin\Menu\MenuShowController@show')->name('adminMiddleMenuShow');
+           Route::get('/chercher', 'Middle\Admin\Menu\MenuShowController@search')->name('adminMiddleMenuSearch');
            Route::get('/creer', 'Middle\Admin\Menu\MenuFormController@show')->name('adminMiddleMenuCreateShow');
         });
 
