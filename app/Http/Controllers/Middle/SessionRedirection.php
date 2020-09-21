04b3e3d3537d681b98b66b8677aada3b41226940
Max <gindre.maxime@gmail.com>
2020-09-21 09:41:31 +0200

    middle-front
    update trait

--- app/Http/Controllers/Middle/SessionRedirection.php
+++ app/Http/Controllers/Middle/SessionRedirection.php
@@ -4,16 +4,34 @@
 namespace App\Http\Controllers\Middle;
 
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
+use App\Repository\StoreRepository;
+use App\Repository\UserRepository;
 
 trait SessionRedirection
 {
+    private StoreRepository $storeRepository;
+
+    private UserRepository $userRepository;
+
+    /**
+     * AdminShowController constructor.
+     *
+     * @param StoreRepository $storeRepository
+     * @param UserRepository $userRepository
+     */
+    public function __construct(StoreRepository $storeRepository, UserRepository $userRepository)
+    {
+        $this->storeRepository = $storeRepository;
+        $this->userRepository = $userRepository;
+    }
+
     public function redirectNoSession()
     {
-        if (!session('store')) {
-            return redirect()->route('adminMiddleShow');
+        $user = $this->userRepository->getStoresByUser(request()->user());
+        $stores = $user->stores;
+
+        if (!request()->session()->get('store')) {
+            request()->session()->put('store', $stores->first());
         }
     }
 }
